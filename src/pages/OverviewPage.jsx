4a74daad9cc1b9684f820d7d57f11f8070c37ad2
Nagham <94856051+Nagham243@users.noreply.github.com>
2025-05-23 01:40:35 +0300

    Update OverviewPage.jsx

--- src/pages/OverviewPage.jsx
+++ src/pages/OverviewPage.jsx
@@ -1,6 +1,6 @@
 import { BarChart2, ShoppingBag, Users, Zap } from "lucide-react";
 import { motion } from "framer-motion";
-
+import { useState, useEffect } from "react";
 import Header from "../components/common/Header";
 import StatCard from "../components/common/StatCard";
 import SalesOverviewChart from "../components/overview/SalesOverviewChart";
